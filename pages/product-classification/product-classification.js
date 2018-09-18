cac0d76f00abab6f128143f75b10610600d152b4
陈微微 <690125575@qq.com>
2018-09-18 18:31:27 +0800

    refactor: 修改样式

--- pages/product-classification/product-classification.js
+++ pages/product-classification/product-classification.js
@@ -17,9 +17,16 @@ Page({
       { name: "分类1", id: 1 },
       { name: "分类1", id: 1 },
       { name: "分类1", id: 1 },
-    ]
+    ],
+    activeIndex:0
   },
   onLoad: function (options) {
-    
+   
+  },
+  search(){
+    Tool.navigateTo('/pages/search/search?door=0')
+  },
+  leftBarClicked(e){
+
   }
 })
