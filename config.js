dfc7dec41c51733ac11229b6b4a3c24584025f92
陈微微 <690125575@qq.com>
2018-09-21 18:20:41 +0800

    refactor:  新增专题页面4个模板 降拍和秒拍未完成

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  // baseUrl:'https://testapi.sharegoodsmall.com/gateway',// 小程序发布的显示地址
+  baseUrl:'https://testapi.sharegoodsmall.com/gateway',// 小程序发布的显示地址
   // baseUrl: 'http://172.16.10.42:8180/gateway',  // 蒋
   // baseUrl: 'http://172.16.10.29:8180/gateway',  // 周
   // baseUrl:'http://172.16.10.238:8180/gateway',  // 高
@@ -7,7 +7,7 @@ const config = {
   // baseUrl: 'http://172.16.10.41:8180/gateway',  // 张
   // baseUrl: 'http://172.16.10.12:8180/gateway',  // 杨
   // baseUrl: 'http://172.16.10.19:8180/gateway',  // 吴
-  baseUrl: 'http://172.16.10.88:8180/gateway',  // 陈
+  // baseUrl: 'http://172.16.10.88:8180/gateway',  // 陈
   wxVersion: 'V2.2.5', // 微信小程序版本
   BaiduMapKey: 'BazNALkeiEEMMvZjWmPeXlRqOyd0BlnL',
   AppId: 'wx228ac7ba52b9b1ed',//小程序AppID
