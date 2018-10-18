ae7cae60f60a2c58423e8046c2654006c3001e7e
陈微微 <690125575@qq.com>
2018-10-18 15:47:31 +0800

    feat: 切换测试地址

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  // baseUrl:'https://testapi.sharegoodsmall.com/gateway',// 测试环境
+  baseUrl:'https://testapi.sharegoodsmall.com/gateway',// 测试环境
   // baseUrl: 'http://devapi.sharegoodsmall.com/gateway',// 开发环境
   // baseUrl: 'http://172.16.10.42:8180/gateway',  // 蒋
   // baseUrl: 'http://172.16.10.29:8180/gateway',  // 周
@@ -7,7 +7,7 @@ const config = {
   // baseUrl:'http://172.16.10.56:8180/gateway',  // 沈
   // baseUrl: 'http://172.16.10.41:8180/gateway',  // 张
   // baseUrl: 'http://172.16.10.12:8180/gateway',  // 杨
-  baseUrl: 'http://172.16.10.19:8180/gateway',  // 吴
+  // baseUrl: 'http://172.16.10.19:8180/gateway',  // 吴
   // baseUrl: 'http://172.16.10.88:8180/gateway',  // 陈
   // baseUrl: 'http://172.16.10.111:8180/gateway',  // 王
   // baseUrl: 'http://zhifu.dnvhot.tech', // 吴
