087bd134663fb056eca013789c5949c4189e5dee
yudeguo <549863285@qq.com>
2018-10-19 18:33:16 +0800

    feat:新增秀购发现首页

---
+++ pages/discover/discover.js
@@ -0,0 +1,39 @@
+Page({
+  data: {
+    tabIndex: 0,
+    discoverData: {},
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+  },
+
+  onLoad (options) {
+
+  },
+  onReady() {
+
+  },
+  onShow() {
+
+  },
+  onHide() {
+
+  },
+  onUnload() {
+
+  },
+  onPullDownRefresh() {
+
+  },
+  onReachBottom() {
+
+  },
+  changeTabIndex(e) {
+    let index = e.currentTarget.dataset.index;
+    this.setData({
+      tabIndex: index
+    })
+  }
+})
