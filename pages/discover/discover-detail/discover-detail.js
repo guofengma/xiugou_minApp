d3813e10a77c158aad6cae0686cc47fa9ef138a0
yudeguo <549863285@qq.com>
2018-10-22 14:20:10 +0800

    feat:新增发现页面相关

--- pages/discover/discover-detail/discover-detail.js
+++ pages/discover/discover-detail/discover-detail.js
@@ -1,16 +1,16 @@
 Page({
+
   data: {
-    tabIndex: 0,
-    discoverData: {},
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
-    showEdit: false
+    assist: false,
+    fav: true,
+    activeIndex: 1
   },
-
-  onLoad (options) {
+  onLoad: function (options) {
 
   },
   onReady() {
@@ -25,16 +25,9 @@ Page({
   onUnload() {
 
   },
-  onPullDownRefresh() {
-
-  },
-  onReachBottom() {
-
-  },
-  changeTabIndex(e) {
-    let index = e.currentTarget.dataset.index;
+  sliderChange(e) {
     this.setData({
-      tabIndex: index
+      activeIndex: e.detail.current + 1
     })
-  }
+  },
 })
---
+++ pages/discover/discover-fav/discover-fav.js
@@ -0,0 +1,31 @@
+Page({
+  data: {
+      showEdit: false
+  },
+  onLoad (options) {
+
+  },
+  onReady () {
+
+  },
+  onShow () {
+
+  },
+  onHide () {
+
+  },
+  onUnload () {
+
+  },
+  onPullDownRefresh () {
+
+  },
+  onReachBottom () {
+
+  },
+  toggleShowEdit() {
+    this.setData({
+      showEdit: !this.data.showEdit
+    })
+  }
+})
