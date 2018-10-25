22234c15855465454466bef9dd6a5b44cc0fccb4
yudeguo <549863285@qq.com>
2018-10-25 15:53:12 +0800

    fixed:修复发现广告位链接跳转错误

--- pages/discover/discover.js
+++ pages/discover/discover.js
@@ -162,8 +162,24 @@ Page({
       tabIndex: index
     })
   },
+  // 轮播跳转 1.链接产品2.链接专题3.降价拍4.秒杀5.礼包 6.外链
   handleSwiperClick(e) {
-    let code = e.currentTarget.dataset.code;
-    Tool.navigateTo('/pages/product-detail/product-detail?prodCode=' + code);
+    let data = e.currentTarget.dataset;
+    let code = data.code;
+    let linkType = data.link
+    let url = '';
+    if(linkType == 1) {
+      url = '/pages/product-detail/product-detail?prodCode=' + code;
+    } else if (linkType == 2) {
+      url = '/pages/topic/topic?code=' + code;
+    } else if (linkType == 3) {
+      url = '/pages/product-detail/discount-detail/discount-detail?code=' + code;
+    } else if (linkType == 4) {
+      url = '/pages/product-detail/seckill-detail/seckill-detail?code=' + code;
+    } else if (linkType == 5) {
+      url = '/pages/product-detail/gift-bag-detail/gift-bag-detail?giftBagId=' + code;
+    } 
+
+    Tool.navigateTo(url);
   }
 })
