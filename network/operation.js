ad6958c6a39f184d0c86781d766da77a091a14df
yudeguo <549863285@qq.com>
2018-11-01 20:43:27 +0800

    任务页面 还差个领取接口以及新用户逻辑

--- network/operation.js
+++ network/operation.js
@@ -558,7 +558,7 @@ export default class Operation {
 
         // 获取刮刮卡
 
-        this.getScratchCard = '/scratch/scratchCard/updateScratchCard'
+        this.getScratchCard = '/scratch/scratchCard/findByCode'
 
         // 获取我的任务列表
 
@@ -582,7 +582,7 @@ export default class Operation {
 
         // 验证刮刮卡使用状态
 
-        this.checkScratchCodeStatus = ''
+        this.checkScratchCodeStatus = '/scratch/scratchCardInformation/findByCode'
 
         __instance(this);
 
