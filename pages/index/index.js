5b9f3958b270dbdf42cb7db3cb89bfa39fe1e398
yudeguo <549863285@qq.com>
2018-11-08 16:52:22 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -96,7 +96,7 @@ Page({
       let r = RequestFactory.wxRequest(params);
       r.successBlock = (req) => {
         let data = req.responseObject.data || {};
-        Tool.navigateTo(`/pages/my/task/task-detail/task-detail?jobId=${data.id}&status=1`)
+        Tool.navigateTo(`/pages/my/task/task-detail/task-detail?jobId=${data.jobId}&status=1&id=${data.id}`)
       };
       Tool.showErrMsg(r)
       r.addToQueue();
