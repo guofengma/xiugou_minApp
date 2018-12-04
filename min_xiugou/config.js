4e2844447018d975ca0c4d181e30727d4a715f28
陈微微 <690125575@qq.com>
2018-12-04 11:11:39 +0800

    feat: 修改加签版本号

--- min_xiugou/config.js
+++ min_xiugou/config.js
@@ -1,11 +1,11 @@
-// 切换 H5 地址 请求地址 加签版本
+// 每次发布要 切换 h5webUrl地址 baseUrl请求地址  rasVersion加签版本
 const config = {
+  rasVersion: '1.0.4.3', //数据加签版本号
+  rasClient: 'miniapp', // 数据加签端口
   // h5webUrl: 'https://uath5.sharegoodsmall.com',// 市场部内测环境
   // baseUrl: 'https://uatapi.sharegoodsmall.com/gateway',// 市场部内测环境
-  // h5webUrl: 'https://testh5.sharegoodsmall.com',
+  h5webUrl: 'https://testh5.sharegoodsmall.com',
   baseUrl:'https://testapi.sharegoodsmall.com/gateway',// 测试环境
-  rasVersion: '1.0.4.3', //数据加签版本号
-  
   // baseUrl: 'http://devapi.sharegoodsmall.com/gateway',// 开发环境
   // baseUrl: 'http://172.16.10.42:4090/gateway',  // 蒋
   // baseUrl: 'http://172.16.10.29:8903/gateway',  // 周
