4db03841116c927a8bd5ff665dd6085a45b372e6
yudeguo <549863285@qq.com>
2018-12-10 11:03:58 +0800

    refactor:修改分享点击经验逻辑

--- min_xiugou/pages/index/index.js
+++ min_xiugou/pages/index/index.js
@@ -147,7 +147,7 @@ Page({
           id: options.inviteId
         })
         //1、产品详情(1,2,101) 2、邀请注册(101) 3、秀场分享(在分享页面那边处理了) 4、拼店分享(暂无): 点击增加经验
-        if ([1, 2, 99, 101].includes(options.type)){
+        if ([1, 2, 99, 101].includes(parseInt(options.type))) {
           app.shareClick(options.inviteId);
         }
       }
